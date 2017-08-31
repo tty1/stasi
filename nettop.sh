11ca47ee8dee89747eb1f51fe5f93de54358788b
tty1 <mail@thomas-rainer.at>
2017-08-31 02:30:37 +0200

    Add files via upload

---
+++ nettop.sh
@@ -0,0 +1,12 @@
+#usr/bin/bash
+
+while [ true ]
+do
+#leep 0.01 && clear && tail -1,2,3,4,5,6,7 | cat
+sleep 3 && clear && cat nettop.log
+#sleep 1 && clear && cat log.txt
+#sleep 1 && clear && tail -n 1 log.txt | cat 
+
+done
+
+exit
